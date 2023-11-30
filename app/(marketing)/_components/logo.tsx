e5b0f07a17cdc8802d871523f34a9cd952df1950
JaedonSpurlock01 <jaedonaspurlock@outlook.com>
2023-11-29 17:16:04 -0800

    Added navbar and dark mode

--- app/(marketing)/_components/logo.tsx
+++ app/(marketing)/_components/logo.tsx
@@ -10,7 +10,20 @@ const font = Poppins({
 export const Logo = () => {
   return (
     <div className="hidden md:flex items-center gap-x-2">
-      <Image src="/logo.svg" width="40" height="40" alt="Logo" />
+      <Image
+        src="/logo.svg"
+        width="40"
+        height="40"
+        alt="Logo"
+        className="dark:hidden"
+      />
+      <Image
+        src="/logo-dark.svg"
+        width="40"
+        height="40"
+        alt="Logo"
+        className="hidden dark:block"
+      />
       <p className={cn("font-semibold", font.className)}>Jotion</p>
     </div>
   );
